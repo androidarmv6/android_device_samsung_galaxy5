3277e60cd39bf4ab4b0fd57a99c840feb6f0a82f
Conn O'Griofa <connogriofa@gmail.com>
2013-01-01 01:04:41 -0200

    Kernel & recovery changes for GT-I5500
    Amend kernel base address
    Adjust BML partitions to bml9,10 (major - can brick without this change)
    Remove tass recovery kernel
    Rename/update recovery_keys.c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,14 +20,17 @@
 include device/samsung/msm7x27-common/BoardConfigCommon.mk
 
 ## Kernel, bootloader
+BOARD_KERNEL_BASE := 0x00200000
 TARGET_BOOTLOADER_BOARD_NAME := galaxy5
 TARGET_KERNEL_CONFIG := cyanogenmod_galaxy5_defconfig
 TARGET_OTA_ASSERT_DEVICE := galaxy5,GT-I5500,GT-I5503,GT-I5508
 
 ## Recovery
+BOARD_BML_BOOT := "/dev/block/bml9"
+BOARD_BML_RECOVERY := "/dev/block/bml10"
 BOARD_LDPI_RECOVERY := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := "<font_7x16.h>"
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxy5/recovery/recovery_ui.c
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxy5/recovery/recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/galaxy5/recovery/graphics.c
 TARGET_RECOVERY_INITRC := device/samsung/galaxy5/recovery/recovery.rc
 TARGET_RECOVERY_FSTAB := device/samsung/galaxy5/recovery/recovery.fstab
