c87aaedc46ee2c39435c3a0c74e93c6b72fc1022
Conn O'Griofa <connogriofa@gmail.com>
2013-10-26 05:54:15 +0100

    Report that we don't support new Android 4.3 BT features to Bluedroid
    Change-Id: I6347e28790fa94e26e3c95d8b47385844bd51283

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,5 +19,8 @@
 
 #define BTM_DEF_LOCAL_NAME   "GT-I5500"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 
 #endif
