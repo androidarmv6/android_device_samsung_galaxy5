3ec45a62b636d8ac756de91b41dd9774d7949a98
Conn O'Griofa <connogriofa@gmail.com>
2014-03-08 04:42:18 +0000

    test
    Change-Id: I569a756d91e5041cab1fffadd9ca2ccd46b11bda

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,11 +28,7 @@ TARGET_PROVIDES_LIBLIGHT := false
 ## Kernel, bootloader
 BOARD_KERNEL_BASE := 0x00200000
 TARGET_BOOTLOADER_BOARD_NAME := galaxy5
-ifdef BUILD_WITH_30X_KERNEL
-	TARGET_KERNEL_CONFIG := cyanogenmod/cyanogenmod_galaxy5_defconfig
-else
-	TARGET_KERNEL_CONFIG := cyanogenmod_galaxy5_defconfig
-endif
+TARGET_KERNEL_CONFIG := cyanogenmod_galaxy5_defconfig
 TARGET_OTA_ASSERT_DEVICE := galaxy5,GT-I5500,GT-I5503,GT-I5508
 
 ## Recovery
